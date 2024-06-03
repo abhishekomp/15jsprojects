800717a1056b4b095b9baf0c9ffbc0ad9e10d730
abhishekomp <abhishek2283@hotmail.com>
2024-06-03 09:52:32 +0200

    scroll project

---
+++ _10_scroll_project/ReadMe.txt
@@ -0,0 +1,5 @@
+Take aways:
+1. Nav bar which is sticky.
+2. Smooth scroll
+3. No hard coding of the year in the footer
+4. Back to Top icon
---
+++ _10_scroll_project/_poc_getBoundingClientRect/app.js
@@ -0,0 +1,4 @@
+const item3 = document.querySelector(".item-2");
+console.log(item3);
+const item3Rect = item3.getBoundingClientRect();
+console.log(item3Rect);
